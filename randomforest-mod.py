2cf3c7e8bbf0804f580d65579e648411aac14758
Yoshio HANAWA <y@hnw.jp>
2016-12-06 22:53:40 +0900

    年齢の推測をマシにした / 意味のありそうなカラムを適当に増やした

---
+++ randomforest-mod.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+""" Writing my first randomforest code.
+Author : AstroDave
+Date : 23rd September 2012
+Revised: 15 April 2014
+please see packages.python.org/milk/randomforests.html for more
+
+""" 
+
+import pandas as pd
+import numpy as np
+import csv as csv
+from sklearn.ensemble import RandomForestClassifier
+
+# Data cleanup
+# TRAIN DATA
+train_df = pd.read_csv('train.csv', header=0)        # Load the train file into a dataframe
+
+# I need to convert all strings to integer classifiers.
+# I need to fill in the missing values of the data and make it complete.
+
+# female = 0, Male = 1
+train_df['Gender'] = train_df['Sex'].map( {'female': 0, 'male': 1} ).astype(int)
+
+# Embarked from 'C', 'Q', 'S'
+# Note this is not ideal: in translating categories to numbers, Port "2" is not 2 times greater than Port "1", etc.
+
+# All missing Embarked -> just make them embark from most common place
+if len(train_df.Embarked[ train_df.Embarked.isnull() ]) > 0:
+    train_df.Embarked[ train_df.Embarked.isnull() ] = train_df.Embarked.dropna().mode().values
+
+Ports = list(enumerate(np.unique(train_df['Embarked'])))    # determine all values of Embarked,
+Ports_dict = { name : i for i, name in Ports }              # set up a dictionary in the form  Ports : index
+train_df.Embarked = train_df.Embarked.map( lambda x: Ports_dict[x]).astype(int)     # Convert all Embark strings to int
+
+# 年齢をPclassとGenderから推測
+median_age = np.zeros((2,3))
+for gd in range(0,2):
+    for pc in range(0,3):
+        median_age[gd, pc] = train_df[(train_df.Gender==gd)&(train_df.Pclass==pc+1)]['Age'].dropna().median()
+        train_df.loc[(train_df.Age.isnull())&(train_df.Gender==gd)&(train_df.Pclass==pc+1), 'Age'] = median_age[gd, pc]
+
+print median_age
+
+train_df['IsOld'] = (train_df.Age > 60).astype(int)
+train_df['IsChildren'] = (train_df.Age < 12).astype(int)
+train_df['IsRich'] = (train_df.Fare > 50.0).astype(int)
+
+# Remove the Name column, Cabin, Ticket, and Sex (since I copied and filled it to Gender)
+train_df = train_df.drop(['Name', 'Sex', 'Ticket', 'Cabin', 'PassengerId'], axis=1) 
+
+# TEST DATA
+test_df = pd.read_csv('test.csv', header=0)        # Load the test file into a dataframe
+
+# I need to do the same with the test data now, so that the columns are the same as the training data
+# I need to convert all strings to integer classifiers:
+# female = 0, Male = 1
+test_df['Gender'] = test_df['Sex'].map( {'female': 0, 'male': 1} ).astype(int)
+
+# Embarked from 'C', 'Q', 'S'
+# All missing Embarked -> just make them embark from most common place
+if len(test_df.Embarked[ test_df.Embarked.isnull() ]) > 0:
+    test_df.Embarked[ test_df.Embarked.isnull() ] = test_df.Embarked.dropna().mode().values
+# Again convert all Embarked strings to int
+test_df.Embarked = test_df.Embarked.map( lambda x: Ports_dict[x]).astype(int)
+
+# All the ages with no data -> make the median of all Ages
+for gd in range(0,2):
+    for pc in range(0,3):
+        test_df.loc[(test_df.Age.isnull())&(test_df.Gender==gd)&(test_df.Pclass==pc+1), 'Age'] = median_age[gd, pc]
+    
+# All the missing Fares -> assume median of their respective class
+if len(test_df.Fare[ test_df.Fare.isnull() ]) > 0:
+    median_fare = np.zeros(3)
+    for f in range(0,3):                                              # loop 0 to 2
+        median_fare[f] = test_df[ test_df.Pclass == f+1 ]['Fare'].dropna().median()
+    for f in range(0,3):                                              # loop 0 to 2
+        test_df.loc[ (test_df.Fare.isnull()) & (test_df.Pclass == f+1 ), 'Fare'] = median_fare[f]
+
+test_df['IsOld'] = (test_df.Age > 60).astype(int)
+test_df['IsChildren'] = (test_df.Age < 12).astype(int)
+test_df['IsRich'] = (test_df.Fare > 50.0).astype(int)
+        
+# Collect the test data's PassengerIds before dropping it
+ids = test_df['PassengerId'].values
+# Remove the Name column, Cabin, Ticket, and Sex (since I copied and filled it to Gender)
+test_df = test_df.drop(['Name', 'Sex', 'Ticket', 'Cabin', 'PassengerId'], axis=1) 
+
+# The data is now ready to go. So lets fit to the train, then predict to the test!
+# Convert back to a numpy array
+train_data = train_df.values
+test_data = test_df.values
+
+
+print 'Training...'
+forest = RandomForestClassifier(n_estimators=100)
+forest = forest.fit( train_data[0::,1::], train_data[0::,0] )
+
+print 'Predicting...'
+output = forest.predict(test_data).astype(int)
+
+
+predictions_file = open("randomforest-mod.csv", "wb")
+open_file_object = csv.writer(predictions_file)
+open_file_object.writerow(["PassengerId","Survived"])
+open_file_object.writerows(zip(ids, output))
+predictions_file.close()
+print 'Done.'
